c40d03940764dd7479367f791dda5ec0e66378b7
TrabalhoTF <aagston@hotmail.com>
2017-06-27 11:17:28 -0300

    atualização fachada

--- src/business/LeilaoFachada.java
+++ src/business/LeilaoFachada.java
@@ -1,6 +1,7 @@
 package business;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import persistence.DaoException;
 
@@ -15,11 +16,16 @@ public class LeilaoFachada {
 	private ArrayList<Produto> listaProdutos;
 	private ArrayList<Lote> listaLote;
 	
+	private HashMap<Integer, Lote> mapId_leilaoLote;
+	
 	
 	public LeilaoFachada() throws DaoException{	
 		listaUsuarios = new ArrayList<Usuario>();
 		listaProdutos = new ArrayList<Produto>();
-		listaLote = new ArrayList<Lote>();				
+		listaLote = new ArrayList<Lote>();			
+		
+		mapId_leilaoLote = new HashMap<Integer, Lote>();
+		
 		throw new DaoException("ERRO AO CRIAR A FACHADA! ");
     }
 	
@@ -67,14 +73,29 @@ public class LeilaoFachada {
 		return listaProdutos.add(produto);		
 	}
 	
-	public boolean criarLote(int id_lote, float preco) throws LeilaoException{
+	public boolean criarLote(int id_leilao, int id_lote, float preco) throws LeilaoException{
 		boolean aux = false;
 		if(ValidadorDados.validarValor(preco)){
 			lote = new Lote(id_lote, preco);
 			aux = listaLote.add(lote);
+			
+			if(mapId_leilaoLote.containsKey(id_leilao)== false){
+				mapId_leilaoLote.put(id_leilao, lote);
+				aux = true;
+			}else{
+				new LeilaoException("Já existe um lote cadastrado para esse leilao!");
+			}	
+			
+		}else{
+			new LeilaoException("Lote não cadastrado! Atribuia a ele um valor válido!");
 		}				
+		
+			
 		return aux;		
 	}
+	
+	
+	
 	
 	public boolean adicionarProdutoLote( int id_lote, int id_produto ){
 		boolean aux = false;
